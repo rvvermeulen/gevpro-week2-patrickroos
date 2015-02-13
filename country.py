1f257efacd5e62993f271cd42b8486dab66e3c22
S2746549 <s2746549@let-edu-145-15.let.rug.nl>
2015-02-13 10:40:10 +0100

    Functie: createCountries() toegevoegd

--- country.py
+++ country.py
@@ -9,3 +9,12 @@ class Country:
 
 	def __str__(self):
 		print("Hello from " + self.name)
+
+def createCountries():
+	source = open("countries_list.txt","r")
+	countrieslist = []
+	for line in source:
+		line.strip()
+		country = Country(line)
+		countrieslist.append(country)
+	return countrieslist
